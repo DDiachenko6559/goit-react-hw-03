45e05243479fb6631c287741262e77f0add1c22d
DDiachenko6559 <153743209+DDiachenko6559@users.noreply.github.com>
2024-07-18 22:40:34 +0300

    searchBox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,17 +1,22 @@
 import s from "./SearchBox.module.css";
+import { useId } from "react";
 
 const SearchBox = ({ value, onChange }) => {
+  const searchBoxId = useId();
+
   const handleChange = (e) => {
     onChange(e.target.value);
   };
 
   return (
     <div className={s.searchBox}>
+      <label htmlFor={searchBoxId}>Find contacts by name</label>
       <input
         type="text"
         placeholder="Search contacts..."
         value={value}
         onChange={handleChange}
+        id={searchBoxId}
       />
     </div>
   );
