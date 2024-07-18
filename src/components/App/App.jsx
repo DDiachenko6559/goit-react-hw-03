90664b3072be6ca65a86039f3ce59476ae68b5d3
DDiachenko6559 <153743209+DDiachenko6559@users.noreply.github.com>
2024-07-18 19:25:20 +0300

    link components to App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ContactList from "../ContactList/ContactList";
+import ContactForm from "../ContactForm/ContactForm";
+import SearchBox from "../SearchBox/SearchBox";
+import s from "./App.module.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+};
+
+export default App;
